61b41efe836eb6e0a580155f1366dcc0b0d39c70
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-03 21:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten (name){
   kittens.push(name)
 }
+function destructivelyPrependKitten (name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten (name){
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten (name){
+  kittens.shift(name)
+}
