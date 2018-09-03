8feafca46a8c227b684741017483d7f3b543bdcd
LoveKosmos <nataliiariabkova@gmail.com>
2018-09-03 21:30:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ function destructivelyRemoveFirstKitten (name){
 function appendKitten(name){
   return [...kittens, name]
 }
+function prependKitten (name){
+  return [ name, ...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
